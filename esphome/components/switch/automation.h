af0d0c71532b931f9adc380e290e01ad08b12e78
Manon Goo <manon@manon.de>
2022-05-20 00:49:39 +0200

    typo

--- esphome/components/switch/automation.h
+++ esphome/components/switch/automation.h
@@ -102,7 +102,7 @@ template<typename... Ts> class IsLockedAction : public Action<Ts...> {
  public:
   explicit IsLockedAction(Switch *a_switch) : switch_(a_switch) {}
 
-  bool play(Ts... x) override { this->switch_->is_locked(); }
+  bool check(Ts... x) override { this->switch_->is_locked(); }
 
  protected:
   Switch *switch_;
